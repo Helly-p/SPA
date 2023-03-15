a0d9c7cef02581246dfe2b6378d7a0a16d4b8699
Helly Prajapati <helly.p@simformsolutions.com>
2023-03-15 14:22:53 +0530

    Script changed.

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-function loadXMLDoc(route){
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(xhttp.responseText);
-        let flag = 0;
-        for (const key in data) {
-            if (route == key) {
-                document.getElementById("title").innerHTML =
-                data[key];
-                flag = 1;
-            }
+async function loadXMLDoc(route) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(xhttp.responseText);
+      for (const key in data) {
+        if (route == key) {
+          loadHTML(data[key]);
+          // alert(file);
         }
-        if(flag == 0){
-            document.getElementById("title").innerHTML =
-            "Error";
-        }
-        window.history.pushState({}, '', route)
       }
-    };
-    xhttp.open("GET", "data.json", true);
-    xhttp.send();
+      window.history.pushState({}, "", route);
+    }
+  };
+  xhttp.open("GET", "data.json", true);
+  xhttp.send();
+}
+
+async function loadHTML(filepath) {
+  const box = document.getElementById("box");
+  const file = await fetch(filepath)
+    .then((res) => res.text())
+    .then((html) => {
+        box.innerHTML = html;
+      }
+    );  
 }
